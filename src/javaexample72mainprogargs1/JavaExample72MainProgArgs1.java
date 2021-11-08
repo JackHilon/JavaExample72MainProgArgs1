f0d2cc94768fef9912e9fd32a79c61caf14b4199
markandmark1111@gmail.com <markandmark1111@gmail.com>
2021-11-08 12:00:42 +0100

    Arguments in the main method.

---
+++ src/javaexample72mainprogargs1/JavaExample72MainProgArgs1.java
@@ -0,0 +1,22 @@
+
+package javaexample72mainprogargs1;
+
+
+public class JavaExample72MainProgArgs1 {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("The number of args arguments: "+args.length);
+        
+        System.out.println("The arguments....");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+        
+        
+        
+        
+    }
+    
+}
